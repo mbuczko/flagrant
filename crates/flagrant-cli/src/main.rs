53664220b0e116a524593e965eabccda0daed62a
Michał Buczko <michal.buczko@gmail.com>
2024-03-06 12:39:56 +0100

    make tracing working

--- crates/flagrant-cli/src/main.rs
+++ crates/flagrant-cli/src/main.rs
@@ -1,15 +1,24 @@
 use axum::Router;
-use tracing_log::LogTracer;
 use tower_http::compression::CompressionLayer;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    LogTracer::init().expect("Failed to set logger");
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| {
+                // axum logs rejections from built-in extractors with the `axum::rejection`
+                // target, at `TRACE` level. `axum::rejection=trace` enables showing those events
+                "flagrant_cli=debug,flagrant=debug,tower_http=debug,axum::rejection=trace".into()
+            }),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
 
     let pool = flagrant::init().await?;
     let app = Router::new()
-        // .route("/@me", get(users::user_identity))
-        // .route_layer(middleware::from_fn(middlewares::add_claim_details))
+    // .route("/@me", get(users::user_identity))
+    // .route_layer(middleware::from_fn(middlewares::add_claim_details))
         .with_state(pool)
         .layer(CompressionLayer::new());
 
@@ -18,7 +27,10 @@ async fn main() -> anyhow::Result<()> {
         .await
         .unwrap();
 
-    tracing::debug!("listening on {}", listener.local_addr().unwrap());
+    // how to set a cookie?
+    // https://github.com/tokio-rs/axum/discussions/351
+    //
+    tracing::info!("listening on {}", listener.local_addr().unwrap());
     axum::serve(listener, app).await.unwrap();
 
     Ok(())
