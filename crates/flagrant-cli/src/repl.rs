b24c0e31c9cf0ca6ac1aa6993b0908a4679aa522
Michał Buczko <michal.buczko@gmail.com>
2024-03-11 14:04:14 +0100

    add sample autocompletion

--- crates/flagrant-cli/src/repl.rs
+++ crates/flagrant-cli/src/repl.rs
@@ -1,6 +1,5 @@
-use std::cmp;
-
 use flagrant::models::project::Project;
+use rustyline::completion::{Completer, Pair};
 use rustyline::error::ReadlineError;
 use rustyline::hint::{Hint, Hinter};
 use rustyline::history::DefaultHistory;
@@ -10,6 +9,8 @@ use rustyline::{Completer, Context, Editor, Helper, Highlighter, Hinter, Result,
 struct ReplHelper {
     #[rustyline(Hinter)]
     hinter: ReplHinter,
+    #[rustyline(Completer)]
+    completer: CommandCompleter,
 }
 
 struct ReplHinter {
@@ -26,7 +27,46 @@ struct CommandHint {
 struct Command {
     command: String,
     hint: String,
-    argc: usize
+    argc: usize,
+}
+
+#[derive(Debug)]
+struct CommandCompleter {
+    candidates: Vec<&'static str>,
+}
+
+impl CommandCompleter {
+    pub fn complete_command(&self, line: &str, pos: usize) -> Result<(usize, Vec<Pair>)> {
+        let len = line.len();
+        let pairs = self
+            .candidates
+            .iter()
+            .filter_map(|c| {
+                if len == 0 || c.starts_with(line) {
+                    return Some(Pair {
+                        display: c.to_string(),
+                        replacement: c[pos..].to_string(),
+                    });
+                }
+                None
+            })
+            .collect::<Vec<_>>();
+
+        Ok((pos, pairs))
+    }
+    pub fn new(commands: Vec<&'static str>) -> CommandCompleter {
+        CommandCompleter {
+            candidates: commands,
+        }
+    }
+}
+
+impl Completer for CommandCompleter {
+    type Candidate = Pair;
+
+    fn complete(&self, line: &str, pos: usize, _ctx: &Context<'_>) -> Result<(usize, Vec<Pair>)> {
+        self.complete_command(line, pos)
+    }
 }
 
 impl Hint for CommandHint {
@@ -49,7 +89,7 @@ impl Command {
         Command {
             command: command.to_lowercase(),
             hint: hint.into(),
-            argc: command.split_whitespace().count() - 1
+            argc: command.split_whitespace().count() - 1,
         }
     }
 }
@@ -62,37 +102,37 @@ impl Hinter for ReplHinter {
             return None;
         }
 
-        let lowered = line[..line.len()-1].to_lowercase();
+        let lowered = line[..line.len() - 1].to_lowercase();
         let argc = line.split_whitespace().count();
-        let command = self.hints
+        let command = self
+            .hints
             .iter()
-            .filter(|candidate| {
-                match candidate.argc {
-                    0 => lowered == candidate.command,
-                    p => argc >= p && lowered.starts_with(candidate.command.as_str())
-                }
+            .filter(|candidate| match candidate.argc {
+                0 => lowered == candidate.command,
+                p => argc >= p && lowered.starts_with(candidate.command.as_str()),
             })
             .next();
 
         if let Some(command) = command {
-            let strip_chars = command.hint
+            let strip_chars = command
+                .hint
                 .chars()
                 .enumerate()
                 .filter(|(_, c)| c.is_whitespace())
                 .map(|(i, _)| i)
                 .nth(argc - 1)
-                .unwrap_or(command.hint.len()-1);
+                .unwrap_or(command.hint.len() - 1);
 
             return Some(CommandHint {
-                display: command.hint[strip_chars+1..].into(),
+                display: command.hint[strip_chars + 1..].into(),
                 complete_up_to: command.command.len().saturating_sub(strip_chars),
-            })
+            });
         }
         None
     }
 }
 
-fn diy_hints() -> Vec<Command> {
+fn repl_hints() -> Vec<Command> {
     let mut hints = Vec::new();
     hints.push(Command::new("help", "help"));
     hints.push(Command::new("env", "env ADD | DEL | LIST | RENAME"));
@@ -102,18 +142,24 @@ fn diy_hints() -> Vec<Command> {
     hints.push(Command::new("feat ADD", "feat ADD feature-name value"));
     hints.push(Command::new("feat DEL", "feat DEL feature-name"));
     hints.push(Command::new("feat VAL", "feat VAL feature-name new-value"));
-    hints.push(Command::new("feat DESC", "feat DESC feature-name new-description"));
+    hints.push(Command::new(
+        "feat DESC",
+        "feat DESC feature-name new-description",
+    ));
     hints.push(Command::new("feat LIST", "feat LIST"));
     hints.push(Command::new("feat", "feat ADD | DEL | DESC | LIST | VAL"));
-
-
 
     hints
 }
 
 pub fn init_repl(project: Project) -> Result<()> {
-    let hinter = ReplHinter { hints: diy_hints() };
-    let helper = ReplHelper { hinter };
+    let hinter = ReplHinter {
+        hints: repl_hints(),
+    };
+    let helper = ReplHelper {
+        hinter,
+        completer: CommandCompleter::new(vec!["feat", "env"]),
+    };
     let mut rl: Editor<ReplHelper, DefaultHistory> = Editor::new()?;
 
     if rl.load_history("history.txt").is_err() {
