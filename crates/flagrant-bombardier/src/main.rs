0401e1b48988f3847475aa968767bdf671b1be94
Michał Buczko <michal.buczko@gmail.com>
2024-06-02 16:00:49 +0200

    use DashMap and DashSet to simplify concurrent access to shared data

--- crates/flagrant-bombardier/src/main.rs
+++ crates/flagrant-bombardier/src/main.rs
@@ -1,41 +1,35 @@
-use std::{
-    collections::{BTreeMap, BTreeSet},
-    sync::{Arc, RwLock},
-    thread,
-};
+use std::{sync::{atomic::{AtomicUsize, Ordering}, Arc}, thread};
 
+use dashmap::{DashMap, DashSet};
 use rand::{rngs::ThreadRng, Rng};
 use ulid::Ulid;
 
 const REQUESTS_COUNT: usize = 1000;
 const THREADS_COUNT: usize = 8;
 
-type LockIdents = Arc<RwLock<Vec<Ulid>>>;
-type LockResults = Arc<RwLock<BTreeMap<String, BTreeSet<String>>>>;
+static IDX: AtomicUsize = AtomicUsize::new(0);
 
 pub fn main() -> anyhow::Result<()> {
-    let idents = Arc::new(RwLock::new(Vec::<Ulid>::with_capacity(REQUESTS_COUNT)));
-    let results = Arc::new(RwLock::new(BTreeMap::<String, BTreeSet<String>>::new()));
+    let idents = Arc::new(DashMap::<usize, Ulid>::with_capacity(REQUESTS_COUNT));
+    let results = Arc::new(DashMap::<String, DashSet<String>>::new());
 
     thread::scope(|s| {
         for _ in 1..=THREADS_COUNT {
-            let idents_clone = idents.clone();
-            let results_clone = results.clone();
+            let idents = Arc::clone(&idents);
+            let results = Arc::clone(&results);
 
             s.spawn(move || {
                 let mut rng = rand::thread_rng();
                 loop {
-                    let id = get_or_generate_id(&idents_clone, &mut rng);
+                    let id = get_or_generate_id(idents.clone(), &mut rng);
                     if let Some(id) = id {
                         let val = get_flag_value(&id);
 
                         // check if user's ID isn't already assigned to other value.
-                        check_and_evict(&results_clone, &id);
+                        evict_from_buckets(results.clone(), &id);
 
                         // add value to corresponding bucket
-                        if let Ok(mut res) = results_clone.write() {
-                            res.entry(val).or_insert(BTreeSet::new()).insert(id);
-                        }
+                        results.entry(val).or_default().insert(id);
                     }
                 }
             });
@@ -44,50 +38,23 @@ pub fn main() -> anyhow::Result<()> {
     Ok(())
 }
 
-/// Generates new or retrieves already existing user's ULID turned into a String.
-/// For performance reasons, provided `idents` vector is read-locked first to check
-/// if ULID already exists. This becomes the most common case after REQUESTS_COUNT
-/// number of requests gets reached.
-///
-/// If no ULID was found, vector is write-relocked for mutation - adding new ULID.
-/// This way either newly created ULID or existing one is being returned without
-/// excessive locking.
-fn get_or_generate_id(idents: &LockIdents, rng: &mut ThreadRng) -> Option<String> {
-    if let Ok(idents_read) = idents.read() {
-        if idents_read.len() >= REQUESTS_COUNT {
-            return idents_read.get(rng.gen_range(0..REQUESTS_COUNT)).map(|id| id.to_string());
-        } else {
-            // drop read-lock and re-enter with read-write lock
-            drop(idents_read);
-            if let Ok(mut idents) = idents.write() {
-                idents.push(ulid::Ulid::new());
-                return idents.last().map(|id| id.to_string());
-            }
-        }
+fn get_or_generate_id(idents: Arc<DashMap<usize, Ulid>>, rng: &mut ThreadRng) -> Option<String> {
+    if idents.len() >= REQUESTS_COUNT {
+        return idents
+            .get(&rng.gen_range(0..REQUESTS_COUNT))
+            .map(|id| id.to_string());
     }
-    None
+    idents
+        .insert(IDX.fetch_add(1, Ordering::SeqCst), ulid::Ulid::new())
+        .map(|id| id.to_string())
 }
 
-fn check_and_evict(results: &LockResults, id: &String) {
-    if let Ok(results_read) = results.read() {
-        for key in results_read.keys() {
-            if results_read.get(key).unwrap().contains(id) {
-                // drop read-lock and re-enter with read-write lock
-                let key_clone = key.clone();
-                drop(results_read);
-
-                if let Ok(mut results_write) = results.write() {
-                    let idents = results_write.get_mut(&key_clone).unwrap();
-                    idents.remove(id);
-                    if idents.is_empty() {
-                        results_write.remove(&key_clone);
-                    }
-                }
-                break;
-            }
-        }
+fn evict_from_buckets(results: Arc<DashMap<String, DashSet<String>>>, id: &String) {
+    for entry in results.iter() {
+        entry.value().remove(id);
     }
 }
+
 fn get_flag_value(_ident: &str) -> String {
     String::from("dupa")
 }
