7f53e47bea6e8f3e67e5549879606f0db218830d
Michal Buczko <michal@buczko.pl>
2024-06-07 21:52:38 +0200

    better types for feature/variants

--- crates/flagrant-types/src/lib.rs
+++ crates/flagrant-types/src/lib.rs
@@ -1,6 +1,6 @@
-use serde::{Deserialize, Serialize};
+use serde::{de::DeserializeOwned, Deserialize, Serialize};
 use serde_valid::Validate;
-use std::fmt;
+use std::{fmt::{self, Display}, marker::PhantomData};
 
 extern crate regex;
 
@@ -21,36 +21,66 @@ pub struct Environment {
 }
 
 #[derive(Debug, Serialize, Deserialize, sqlx::FromRow, Validate)]
-pub struct Feature {
+pub struct Feature<T: FeatureValueType> {
+    #[sqlx(rename = "value_type")]
+    pub _type: PhantomData<T>,
+
     #[sqlx(rename = "feature_id")]
     pub id: u16,
     pub project_id: u16,
     #[validate(pattern = r"^[A-Za-z][A-Za-z0-9_]+$")]
     #[validate(max_length = 255)]
     pub name: String,
-    pub variants: Vec<Variant>,
-    pub value_type: FeatureValueType,
+    pub variants: Vec<Variant<T>>,
     pub is_enabled: bool,
 }
 
-#[derive(Clone, Default, Debug, PartialEq, Serialize, Deserialize, sqlx::Type)]
-#[sqlx(type_name = "value_type", rename_all = "lowercase")]
-// #[serde(rename_all = "lowercase")]
-pub enum FeatureValueType {
-    #[default]
-    Text,
-    Json,
-    Toml,
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+pub struct FeatureValue<T: FeatureValueType>(T, String);
+
+pub trait FeatureValueType {
+    fn discriminator(&self) -> &'static str;
+}
+pub struct JsonType {}
+pub struct TomlType {}
+pub struct TextType {}
+
+impl FeatureValueType for JsonType {
+    fn discriminator(&self) -> &'static str {
+        "json"
+    }
+}
+impl FeatureValueType for TomlType {
+    fn discriminator(&self) -> &'static str {
+        "toml"
+    }
+}
+impl FeatureValueType for TextType {
+    fn discriminator(&self) -> &'static str {
+        "text"
+    }
 }
 
-#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
-pub struct FeatureValue(pub String, pub FeatureValueType);
+// #[derive(Clone, Default, Debug, PartialEq, Serialize, Deserialize, sqlx::Type)]
+// #[sqlx(type_name = "value_type", rename_all = "lowercase")]
+// // #[serde(rename_all = "lowercase")]
+// #[derive(Debug)]
+// pub enum StringType {
+//     // #[default]
+//     Text,
+//     Json,
+//     Toml,
+// }
 
-#[derive(Clone, Debug, Serialize, Deserialize, Eq, PartialEq, sqlx::FromRow)]
-pub struct Variant {
+
+// #[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+// pub struct FeatureValue(pub String, pub FeatureValueType);
+
+#[derive(Clone, Debug, Serialize, Deserialize, sqlx::FromRow)]
+pub struct Variant<T: FeatureValueType> {
     #[sqlx(rename = "variant_id")]
     pub id: u16,
-    pub value: String,
+    pub value: FeatureValue<T>,
     pub weight: i16,
     pub accumulator: i32,
     pub environment_id: Option<u16>,
@@ -63,9 +93,9 @@ pub struct EnvRequestPayload {
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-pub struct FeatureRequestPayload {
+pub struct FeatureRequestPayload<T: FeatureValueType> {
     pub name: String,
-    pub value: Option<FeatureValue>,
+    pub value: Option<FeatureValue<T>>,
     pub description: Option<String>,
     pub is_enabled: bool,
 }
@@ -76,31 +106,32 @@ pub struct VariantRequestPayload {
     pub weight: i16,
 }
 
-impl From<Feature> for FeatureRequestPayload {
-    fn from(feature: Feature) -> Self {
+impl<T: FeatureValueType> From<Feature<T>> for FeatureRequestPayload<T> {
+    fn from(feature: Feature<T>) -> Self {
         FeatureRequestPayload {
             name: feature.name,
             value: feature
                 .variants
-                .first()
-                .map(|v| FeatureValue(v.value.clone(), feature.value_type)),
+                .into_iter()
+                .find(|v| v.environment_id.is_some())
+                .map(|v| v.value),
             description: None,
             is_enabled: feature.is_enabled,
         }
     }
 }
 
-impl Feature {
-    pub fn get_default_variant(&self) -> Option<&Variant> {
+impl<T: FeatureValueType> Feature<T> {
+    pub fn get_default_variant(&self) -> Option<&Variant<T>> {
         self.variants.iter().find(|v| v.is_control())
     }
-    pub fn get_default_value(&self) -> Option<FeatureValue> {
+    pub fn get_default_value(&self) -> Option<FeatureValue<T>> {
         if let Some(variant) = self.get_default_variant() {
-            return Some(FeatureValue(variant.value.clone(), self.value_type.clone()));
+            return Some(variant.value);
         }
         None
     }
-    pub fn with_variants(mut self, variants: Vec<Variant>) -> Self {
+    pub fn with_variants(mut self, variants: Vec<Variant<T>>) -> Self {
         self.variants = variants;
         self
     }
@@ -130,27 +161,27 @@ impl Variant {
     }
 }
 
-impl fmt::Display for FeatureValueType {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{:?}", self)
-    }
-}
+// impl fmt::Display for FeatureValueType {
+//     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+//         write!(f, "{:?}", self)
+//     }
+// }
 
-impl From<&str> for FeatureValueType {
-    fn from(value: &str) -> Self {
-        match value {
-            "json" => Self::Json,
-            "toml" => Self::Toml,
-            _ => Self::default(),
-        }
-    }
-}
+// impl From<&str> for FeatureValueType {
+//     fn from(value: &str) -> Self {
+//         match value {
+//             "json" => Self::Json,
+//             "toml" => Self::Toml,
+//             _ => Self::default(),
+//         }
+//     }
+// }
 
 pub trait Tabular {
     fn tabular_print(&self);
 }
 
-impl Tabular for Feature {
+impl<T: FeatureValueType> Tabular for Feature<T> {
     fn tabular_print(&self) {
         let toggle = if self.is_enabled { "▣" } else { "▢" };
         let value = self
@@ -174,7 +205,7 @@ impl Tabular for Feature {
     }
 }
 
-impl Tabular for Variant {
+impl<T: FeatureValueType> Tabular for Variant<T> {
     fn tabular_print(&self) {
         println!(
             "│ {:<8}: {}\n│ {:<8}: {}\n│ {:<8}: {}",
