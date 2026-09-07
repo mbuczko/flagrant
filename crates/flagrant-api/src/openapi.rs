5d7b337d419a35c1eae67d385cdaf96c015a9356
Michal Buczko <michal@buczko.pl>
2026-09-07 12:30:42 +0200

    bump up version to 0.0.30

--- crates/flagrant-api/src/openapi.rs
+++ crates/flagrant-api/src/openapi.rs
@@ -118,7 +118,7 @@ use utoipa::OpenApi;
     ),
     info(
         title = "Flagrant API",
-        version = "0.0.29",
+        version = "0.0.30",
         description = "CLI-powered feature-flagging service"
     )
 )]
